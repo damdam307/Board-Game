a1ffb845a49d4b928dd4da4eb6f63f04777bea13
Alessandro Damiani <damiani.ale07@gmail.com>
2025-10-29 21:54:40 +0100

    .

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
-b = [[-1]*9]*9
-p = [[1]*9]*4
-l = [{}*4]
+import random as r
+
+b = [[-1 for _ in range(9)] for _ in range(9)]
+p = [[1 for _ in range(9)] for _ in range(4)]
+l = [set(),set(),set(),set()]
 pool = [27]*4
 score = [0]*4
 
@@ -20,7 +22,8 @@ def premove(pl,x):
         pool[pl]-=1
     if p[pl][x] != 3:
         p[pl][x]+=1
-        l.add((x,-1))
+        if p[pl][x]==3:
+            l[pl].add((x,-1))
     else:
         return -1
     return 0
@@ -28,23 +31,24 @@ def premove(pl,x):
 def move(pl,x,y,dx,dy):
     t = getPos(pl,x,y,dx,dy)
     b[t[0]][t[1]]=pl
-    l.add((x+dx,y+dy))
+    l[pl].add((x+dx,y+dy))
 
 def moveFromPre(pl,x,y,dx,dy):
     p[pl][x]=0
+    l[pl].remove((x,y))
     move(pl,x,y,dx,dy)
 
 def moveOnBoard(pl,x,y,dx,dy):
-    t = getPos(pl,x,y,dx,dy)
+    t = getPos(pl,x,y,0,0)
     l[pl].remove((x,y))
     b[t[0]][t[1]]=-1
     move(pl,x,y,dx,dy)
 
 def checkMovesSingle(pl,x,y):
-    m = [[]]*3
+    m = [[],[],[]]
     for i in [-1,0,1]:
-        t = getPos(pl,x,y,2*i,2)   
-        if t[0]>8 or t[0]<0:
+        t = getPos(pl,x,y,i,1)   
+        if x+i>8 or x+i<0:
             continue
         if y+1>8 or b[t[0]][t[1]]==-1:
             m[0].append((i,1))
@@ -52,7 +56,7 @@ def checkMovesSingle(pl,x,y):
             if b[t[0]][t[1]]!=pl:
                 m[1].append((i,1))
             t1=getPos(pl,x,y,2*i,2)
-            if t1[0]>8 or t1[0]<0:
+            if x+2*i>8 or x+2*i<0:
                 continue
             if y+2>8 or b[t1[0]][t1[1]]==-1:
                 m[2].append((2*i,2))
@@ -62,7 +66,9 @@ def checkMovesSingle(pl,x,y):
 def checkMovesAll(pl):
     m = []
     for pos in l[pl]:
-        m.append((pos,checkMovesSingle(pl,pos[0],pos[1])))
+        el = checkMovesSingle(pl,pos[0],pos[1])
+        if el != [[],[],[]]:
+            m.append((pos,el))
     return m
 
 def checkPremoves(pl):
@@ -70,12 +76,26 @@ def checkPremoves(pl):
     for x in range(9):
         if pool!=0 or (p[pl][x] in [1,2]):
             m.append(x)
+    return m
 
 def choosemove(pl,m):
-    pass
+    el = m[r.randint(0,len(m)-1)]
+    ln = 0
+    delet = 0
+    tp = []
+    while ln==0:
+        tp = el[1][r.randint(0,len(el[1])-1)]
+        ln = len(tp)
+    if tp == el[1][1]:
+        delet = 1
+    elm = tp[r.randint(0,len(tp)-1)]
+    return (delet,(el[0],elm))
 
 def choosepremove(pl,m,avoid):
-    pass
+    res = avoid
+    while res==avoid:
+        res = m[r.randint(0,len(m)-1)]
+    return res
 
 turn = 0
 passedTurns=0
@@ -84,20 +104,30 @@ while passedTurns<4:
     avoid = -1
     if len(moves)==0:
         pmoves = checkPremoves(turn%4)
-        if len(moves)==0:
+        if len(pmoves)==0:
             passedTurns+=1
             turn+=1
             continue
         avoid = choosepremove(turn%4,pmoves,avoid)
         premove(turn%4,avoid)
-    else
+    else:
         mm = choosemove(turn%4,moves)
         if mm[0]==1:
             for ll in l:
                 ll.discard(getPos(turn%4,mm[1][0][0],mm[1][0][1],mm[1][1][0],mm[1][1][1]))
-            if mm[1][0][1]==-1:
-                moveFromPre(turn%4,mm[1][0][0],mm[1][0][1],mm[1][1][0],mm[1][1][1])
-            elif mm[1][0][1]+mm[1][1][1]>8:
-
-            else:
-                moveOnBoard(turn%4,mm[1][0][0],mm[1][0][1],mm[1][1][0],mm[1][1][1])
+        if mm[1][0][1]==-1:
+            moveFromPre(turn%4,mm[1][0][0],mm[1][0][1],mm[1][1][0],mm[1][1][1])
+        elif mm[1][0][1]+mm[1][1][1]>8:
+            l[turn%4].remove((mm[1][0][0],mm[1][0][1]))
+            b[mm[1][0][0]][mm[1][0][1]]=-1
+            score[turn%4]+=1
+        else:
+            moveOnBoard(turn%4,mm[1][0][0],mm[1][0][1],mm[1][1][0],mm[1][1][1])
+    pmoves = checkPremoves(turn%4)
+    if len(pmoves)==0:
+        turn+=1
+        continue
+    avoid = choosepremove(turn%4,pmoves,avoid)
+    premove(turn%4,avoid)
+    turn+=1
+    print(pool,end='\r')
