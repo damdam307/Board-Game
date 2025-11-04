b6efc3036839c4c79608f736969462eefcb498ab
Alessandro Damiani <damiani.ale07@gmial.com>
2025-11-04 11:25:12 +0000

    changed for testing

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import Crosswar as c
 import time
-g = c.Game()
 l = '\r'+'\033[1A'*15
-print('\n'*15)
-while g.advanceGame() != 1:
-    print(l+g.write())
-    time.sleep(0.1)
-print(g.getScore())
+#print('\n'*15)
+s = [0,0,0,0]
+for i in range(100):
+    print(i,end='\r')
+    g = c.Game()
+    while g.advanceGame() != 1:
+        pass
+        #print(l+g.write())
+    a = g.getScore()
+    s[0]+=a[0]
+    s[1]+=a[1]
+    s[2]+=a[2]
+    s[3]+=a[3]
+print(s)
